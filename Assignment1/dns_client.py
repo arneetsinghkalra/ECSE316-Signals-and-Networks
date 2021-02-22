3f233ba34fcc2b60be1cdaa588a21451c6fcea5a
arneetsinghkalra <a.s.kalra1999@gmail.com>
2021-02-22 18:06:43 -0500

    Takes input from command line

--- Assignment1/dns_client.py
+++ Assignment1/dns_client.py
@@ -2,19 +2,59 @@ import time
 import socket
 import packet_constructor
 import packet_deconstructor
+import argparse
+
 
 def main():
-    # default values
-    timeout = 5
-    max_retries = 3
-    port = 53
-    type = 'NS'
-    server = '8.8.8.8'          # example query
-    name = 'www.google.ca'      # example query
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="python dns_client.py [-t timeout] [-r max-retries] [-p port] ["
+                                                 "-mx|-ns] @server name")
+    # Adding optional arguments
+    parser.add_argument("-t", "--timeout", type=int, dest='timeout', default=5,
+                        help="timeout (optional) gives how long to wait, in seconds, before retransmitting an "
+                             "unanswered query. Default value: 5."
+                        )
+    parser.add_argument("-r", "--maxretries", type=int, dest='maxretries', default=3,
+                        help="max-retries(optional) is the maximum number of times to retransmit an "
+                             "unanswered query before giving up. Default value: 3."
+                        )
+    parser.add_argument("-p", "--port", type=int, dest='port', default=53,
+                        help="port (optional) is the UDP port number of the DNS server. Default value: 53."
+                        )
+    # Adding mutually exclusive optional parameters
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-mx", action='store_true',
+                       help='mx/ns flags (optional) indicate whether to send a MX (mail server) or \
+                                NS (name server) query. At most one of these can be given, and if neither \
+                                is given then the client should send a type A (IP address) query')
+    group.add_argument("-ns", action='store_true')
+
+    parser.add_argument("server", type=str,
+                        help="server (required) is the IPv4 address of the DNS server, in a.b.c.d. format")
+    parser.add_argument("name", type=str,
+                        help="name (required) is the domain name to query for")
+
+    args = parser.parse_args()
+
+    # Store Values
+    timeout = args.timeout
+    max_retries = args.maxretries
+    port = args.port
+    if args.mx:
+        type = 'MX'
+    elif args.ns:
+        type = 'NS'
+    else:
+        type = 'A'
+    server = args.server
+    name = args.name.strip()
 
     print("dns_client sending request for {}.".format(name))
     print("Server: {}".format(server))
     print("Request type: {}".format(type))
+    print("Timeout: {}".format(timeout))
+    print("Max Retries: {}".format(max_retries))
+    print("Port: {}".format(port))
 
     # Create socket connection then try to send a packet and receive the reply
     for retries in range(max_retries):
@@ -37,6 +77,7 @@ def main():
             print("ERROR: Request timed out.")
 
     packet_deconstructor.deconstruct_packet(recvB, packet_constructor.get_qname(name))
+
 
 if __name__ == "__main__":
     main()
