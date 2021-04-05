2cb06d02422cc4b7b9b49736def120452219e6d2
David Gul <david.gul@icloud.com>
2021-04-05 14:35:10 -0400

    Add fft

--- Assignment2/fft.py
+++ Assignment2/fft.py
@@ -1,58 +1,112 @@
 import argparse
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import cv2
 import time
-import fouriertransform
 import math
+import fouriertransform
 
 def main():
-    args = get_args() 
-    #Store values
+    args = get_args()
+    # Store values 
     mode = args.mode
     image_path = args.image
-
-    # Get Image, grayscale so it is a 2D Array
+    # Get grayscale image
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Resize image if need be
-    image = resize_image(image)
-
     if mode == 1:
-        run_fft()
+        run_fft(image)
     if mode == 2:
-        run_denoised_fft()
+        denoise(image)
     if mode == 3:
-        compress()
+        compress(image)
     if mode == 4:
         produce_plots()
-    
 
-def run_fft():
-    print("run fft")
-    
-def run_denoised_fft():
-    print("run denoised fft")
+def run_fft(image):
+    # Obtain fft 2d
+    fft_image = fouriertransform.fft_2d(pad_image(image))
 
-def compress():
-    print("compress")
+    # Plot images
+    figure, axes = plt.subplots(1, 2)
+    axes[0].imshow(image, plt.cm.gray)
+    axes[1].imshow(np.abs(fft_image), norm=colors.LogNorm())
+    plt.show()
+    
+def denoise(image):
+    # Obtain fft_2d
+    fft_image = fouriertransform.fft_2d(pad_image(image))
+
+    # Remove high values
+    ratio = 0.1
+    width, height = fft_image.shape
+    fft_image[int(ratio * width) : int((1 - ratio) * width)] = 0
+    fft_image[:, int(ratio * height) : int((1 - ratio) * height)] = 0
+
+    # Obtain inverse_fft_2d
+    inverse_fft_image = fouriertransform.inverse_fft_2d(fft_image).real
+
+    # Plot images
+    figure, axes = plt.subplots(1, 2)
+    axes[0].imshow(image, plt.cm.gray)
+    axes[1].imshow(inverse_fft_image[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    plt.show()
+
+def compress(image):
+    # Obtain fft_2d
+    fft_image = fouriertransform.fft_2d(pad_image(image))
+
+    # Compress
+    compress_factors = [0, 10, 25, 50, 75, 95]
+    compressed_images = compress_image(image, fft_image, compress_factors)
+
+    # Plot image compressions
+    figure, axes = plt.subplots(2, 3)
+    axes[0, 0].imshow(np.real(compressed_images[0])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    axes[0, 1].imshow(np.real(compressed_images[1])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    axes[0, 2].imshow(np.real(compressed_images[2])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    axes[1, 0].imshow(np.real(compressed_images[3])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    axes[1, 1].imshow(np.real(compressed_images[4])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    axes[1, 2].imshow(np.real(compressed_images[5])[:image.shape[0], :image.shape[1]], plt.cm.gray)
+    plt.show()
+
+def compress_image(image, fft_image, compressed):
+    # Get original image size
+    size = image.shape[0] * image.shape[1]
+    compressed_images = []
+    inversed_compressed_images = []
+    
+    # Compress image by the appropriate factors
+    for i in compressed:
+        non_zero = int(size * (100 - i) / 100)
+        print("Number of nonzero Fourier coefficients for {}% compression: {}".format(i, non_zero))
+        low = np.percentile(fft_image, (100 - i) // 2)
+        high = np.percentile(fft_image, 100 - (100 - i) // 2)
+        compressed_images.append(fft_image * np.logical_or(fft_image <= low, fft_image >= high))
+    
+    # Obtain inverse_fft_2d
+    for i in compressed_images:
+        inversed_compressed_images.append(fouriertransform.inverse_fft_2d(i))
+
+    return inversed_compressed_images
 
 def produce_plots():
-
     # Generate Arrays of Random numbers
-    test_arrays = [np.random.random((2 ** 5, 2 ** 5)),
-              np.random.random((2 ** 6, 2 ** 6)),
-              np.random.random((2 ** 7, 2 ** 7)),         # DFT Mean around 5.5s, var around 0.0119s
-              np.random.random((2 ** 8, 2 ** 8))          # DFT Mean around 42.838s, var around 1.19s
-              #np.random.random((2 ** 9, 2 ** 9)),        # DFT Mean around 372.367s, var around 5.299s (Super long)
-              #np.random.random((2** 10, 2** 10))         # (Going to take way too long to run)
-              ]
-
+    test_arrays = [
+        np.random.random((2 ** 5, 2 ** 5)),
+        np.random.random((2 ** 6, 2 ** 6)),
+        np.random.random((2 ** 7, 2 ** 7)),         # DFT Mean around 5.5s, var around 0.0119s
+        np.random.random((2 ** 8, 2 ** 8))          # DFT Mean around 42.838s, var around 1.19s
+        # np.random.random((2 ** 9, 2 ** 9)),       # DFT Mean around 372.367s, var around 5.299s (Super long)
+        # np.random.random((2** 10, 2** 10))        # (Going to take way too long to run)
+    ]
     # Store results
     dimensions_array = []
     dft_mean_array = []
     dft_variance_array = []
+    fft_mean_array = []
+    fft_variance_array = []
 
     for array in test_arrays:
         # Store problem size and append to designated array
@@ -60,46 +114,48 @@ def produce_plots():
         dimensions_array.append(dimension)
 
         dft_results = []
-        # fft_rssults = []
-        
-
-        for i in range(1, 10):
+        fft_results = []
+        for i in range(10):
             # Naive DFT Method
             start_time = time.time()
             fouriertransform.dft_2d(array)
             end_time = time.time()
             dft_results.append(end_time - start_time)
 
-            # TODO: FFT Timing here
-            # Naive DFT Method
-            #start_time = time.time()
-            #fouriertransform.fft_2d(array)
-            #end_time = time.time()
-            #fft_results.append(end_time - start_time)
+            # FFT Method
+            start_time = time.time()
+            fouriertransform.fft_2d(array)
+            end_time = time.time()
+            fft_results.append(end_time - start_time)
         
-        # Store mean and variance
+        # Store dft mean and variance
         dft_mean = np.mean(dft_results)
-        dft_variance = np.var(dft_results)
-
         dft_mean_array.append(dft_mean)
+        dft_variance = np.var(dft_results)
         dft_variance_array.append(dft_variance)
 
+        # Store fft mean and variance
+        fft_mean = np.mean(fft_results)
+        fft_mean_array.append(fft_mean)
+        fft_variance = np.var(fft_results)
+        fft_variance_array.append(fft_variance)
 
         # Print mean and variance
         print('Array Dimensions: {} by {}'.format(dimension, dimension))
         print("----------------------------------------")
         print("DFT Mean: ", dft_mean)
+        print("FFT Mean: ", np.mean(fft_results))
         print("DFT Variance: ", dft_variance)
-
-        #print("FFT Mean: ", np.mean(fft_results))
-        #print("FFT Variance: ", np.var(fft_results))
+        print("FFT Variance: ", np.var(fft_results))
         print("----------------------------------------\n")
 
     # Plot Results
     # Error is standard deviation * 2
-    errors = [math.sqrt(i) * 2 for i in dft_variance_array]
+    dft_errors = [math.sqrt(i) * 2 for i in dft_variance_array]
+    fft_errors = [math.sqrt(i) * 2 for i in fft_variance_array]
 
-    plt.errorbar(dimensions_array, dft_mean_array, yerr = errors, label='DFT')
+    plt.errorbar(dimensions_array, dft_mean_array, yerr = dft_errors, label='DFT')
+    plt.errorbar(dimensions_array, fft_mean_array, yerr = fft_errors, label='FFT')
     plt.title('Mean Time vs Problem Size')
     plt.xlabel('Problem Size', fontsize=14)
     plt.ylabel('Runtime (s)', fontsize=14)
@@ -107,39 +163,31 @@ def produce_plots():
     plt.grid(True)
     plt.show()
 
+def pad_image(image):
+    width = int(pow(2, math.ceil(math.log2(image.shape[0]))))
+    height = int(pow(2, math.ceil(math.log2(image.shape[1]))))
 
+    new_shape = width, height
+    new_image = np.zeros(new_shape)
+    new_image[:image.shape[0], :image.shape[1]] = image
 
-# Additional Helper Methods:
-def resize_image(image):
-    width, height = image.shape
-
-    # If width is not a power of 2
-    if not (width and (not (width & (width - 1)))):
-        # Change value to next power of 2
-        width = int(2 ** np.ceil(np.log2(width)))
-
-    # If height is not a power of 2
-    if not (height and (not (height & (height - 1)))):
-        height = int(2 ** np.ceil(np.log2(height)))
-    
-    # Resize Image
-    return cv2.resize(image, (width,height))
-
+    return new_image
 
 def get_args():
     # Initialize parser
     parser = argparse.ArgumentParser(description="python fft.py [-m mode] [-i image]")
 
     # Adding optional arguments
-    parser.add_argument("-m", "--mode", type=int, dest='mode', default=1,
-                        help="- [1] (Default) for fast mode where the image is converted into its FFT form and displayed - [2] for denoising where the image is denoised by applying an FFT, truncating high frequencies and then displayed - [3] for compressing and saving the image- [4] for plotting the runtime graphs for the report"
-                        )
-    parser.add_argument("-i", "--image", dest='image', default = "moonlanding.png",
-                        help="image (optional): filename of the image we wish to take the DFT of. (Default: the file name of the image given to you for the assignment)"
-                        )
+    parser.add_argument(
+        "-m", "--mode", type=int, dest='mode', default=1,
+        help="- [1] (Default) for fast mode where the image is converted into its FFT form and displayed - [2] for denoising where the image is denoised by applying an FFT, truncating high frequencies and then displayed - [3] for compressing and saving the image - [4] for plotting the runtime graphs for the report"
+    )
+    parser.add_argument(
+        "-i", "--image", dest='image', default = "moonlanding.png",
+        help="image (optional): filename of the image we wish to take the DFT of. (Default: the file name of the image given to you for the assignment)"
+    )
 
     return parser.parse_args()
-
 
 if __name__ == "__main__":
     main()
--- Assignment2/fouriertransform.py
+++ Assignment2/fouriertransform.py
@@ -2,9 +2,9 @@ import numpy as np
 
 def dft(signal):
     signal = np.asarray(signal, dtype=complex) # Put input as array with complex type
-    N = signal.shape[0]   #Return number of rows in array
+    N = signal.shape[0]   # Return number of rows in array
 
-    #Instantiate array of zeros of length rows with complex type
+    # Instantiate array of zeros of length rows with complex type
     dft = np.zeros(N, dtype=complex)
 
     for k in range(N):
@@ -27,7 +27,6 @@ def inverse_dft(X):
 
     return inverse_dft
 
-#Implementing 2d dft and inverse to use for mode 4
 def dft_2d(signal):
     signal = np.asarray(signal, dtype=complex)
 
@@ -35,17 +34,17 @@ def dft_2d(signal):
     N = signal.shape[0]
     M = signal.shape[1]
 
-    #Instantate arrays of 0s with same dimensions 
+    # Instantate arrays of 0s with same dimensions 
     row = np.zeros((N, M), dtype=complex) 
     dft_2d = np.zeros((M, N), dtype=complex) 
     
-    # Compute the DFT of input and store it in row
+    # Compute the dft of input and store it in row
     for n in range(N):
         row[n] = dft(signal[n])
 
     column = row.transpose()
 
-    # Compute DFT of transposed array
+    # Compute dft of transposed array
     for m in range(M):
         dft_2d[m] = dft(column[m])
 
@@ -57,7 +56,7 @@ def inverse_dft_2d(X):
     N = X.shape[0]
     M = X.shape[1]
 
-    #Instantate arrays of 0s with same dimensions 
+    # Instantate arrays of 0s with same dimensions 
     row = np.zeros((N, M), dtype=complex)
     inverse_dft_2d = np.zeros((M, N), dtype=complex)
 
@@ -66,8 +65,64 @@ def inverse_dft_2d(X):
 
     column = row.transpose()
 
-    # Compute DFT of transposed array
+    # Compute dft of transposed array
     for m in range(M):
         inverse_dft_2d[m] = inverse_dft(column[m])
 
     return inverse_dft_2d.transpose()
+
+def fft(signal):
+    threshold = 16
+    signal = np.asarray(signal, dtype=complex)
+    N = signal.shape[0]
+
+    if N > threshold:
+        even = fft(signal[::2])
+        odd = fft(signal[1::2])
+        result = np.zeros(N, dtype=complex)
+        for n in range(N):
+            result[n] = even[n % (N // 2)] + np.exp(-2j * np.pi * n / N) * odd[n % (N // 2)]
+        return result
+    else:
+        return dft(signal)
+
+def inverse_fft(signal):
+    threshold = 16
+    signal = np.asarray(signal, dtype=complex)
+    N = signal.shape[0]
+
+    if N > threshold:
+        even = inverse_fft(signal[::2])
+        odd = inverse_fft(signal[1::2])
+        result = np.zeros(N, dtype=complex)
+        for n in range(N):
+            result[n] = (N // 2) * even[n % (N // 2)] + np.exp(2j * np.pi * n / N) * (N // 2) * odd[n % (N // 2)]
+            result[n] = result[n] / N
+
+        return result
+    else:
+        return inverse_dft(signal)
+
+def fft_2d(signal):
+    signal = np.asarray(signal, dtype=complex)
+    width, height = signal.shape
+    result = np.zeros((width, height), dtype=complex)
+
+    for i in range(height):
+        result[:, i] = fft(signal[:, i])
+    for i in range(width):
+        result[i, :] = fft(result[i, :])
+
+    return result
+
+def inverse_fft_2d(signal):
+    signal = np.asarray(signal, dtype=complex)
+    width, height = signal.shape
+    result = np.zeros((width, height), dtype=complex)
+
+    for i in range(width):
+        result[i, :] = inverse_fft(signal[i, :])
+    for i in range(height):
+        result[:, i] = inverse_fft(result[:, i])
+
+    return result
